e51d01fd87e1ef00969b9f613f397f9ca2681550
Diego Lapiduz <diego.lapiduz@gsa.gov>
2016-03-22 20:50:43 -0500

    add nessus agent job and package

---
+++ src/apt/nessus-agent/profile.sh
@@ -0,0 +1,5 @@
+export PATH="/var/vcap/packages/nessus-agent/apt/usr/bin:$PATH"
+export LD_LIBRARY_PATH="/var/vcap/packages/nessus-agent/apt/usr/lib:${LD_LIBRARY_PATH:-}"
+export INCLUDE_PATH="/var/vcap/packages/nessus-agent/apt/usr/include:${INCLUDE_PATH:-}"
+export CPATH="$INCLUDE_PATH"
+export CPPPATH="$INCLUDE_PATH"
