e37b75ffa9949144bebaa224a8c20210ff4e4528
Chris Nelson <christopher.nelson@gsa.gov>
2016-12-20 12:05:00 -0800

    attempt to relink the agent every 30 minutes

---
+++ jobs/nessus-agent/templates/bin/link-agent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# "nessuscli agent status" will exit code 2 if the agent is not linked
+if ! /opt/nessus_agent/sbin/nessuscli agent status; then
+    /opt/nessus_agent/sbin/nessuscli agent link \
+    --host=<%= p("nessus-agent.server") %> \
+    --port=<%= p("nessus-agent.port") %> \
+    --key=<%= p("nessus-agent.key") %> \
+    --name=<%= name %>-<%= spec.index %>-<%= p("nessus-agent.group") %> \
+    --groups=<%= p("nessus-agent.group") %> || true
+fi
