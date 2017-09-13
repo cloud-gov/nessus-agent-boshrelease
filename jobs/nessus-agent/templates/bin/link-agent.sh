ccd2fa49897941e15ee6c21d88cf5e8ef02c2d0a
Russ Thompson <vigornj@gmail.com>
2017-09-13 15:46:15 -0400

    remove groups from hostname
    remove groups from hostname, fixes UI display issue with long hostnames

--- jobs/nessus-agent/templates/bin/link-agent.sh
+++ jobs/nessus-agent/templates/bin/link-agent.sh
@@ -6,6 +6,6 @@ if ! /opt/nessus_agent/sbin/nessuscli agent status; then
     --host=<%= p("nessus-agent.server") %> \
     --port=<%= p("nessus-agent.port") %> \
     --key=<%= p("nessus-agent.key") %> \
-    --name=<%= spec.deployment %>-<%= name %>-<%= spec.index %>-<%= p("nessus-agent.group") %> \
+    --name=<%= spec.deployment %>-<%= name %>-<%= spec.index %> \
     --groups=<%= p("nessus-agent.group") %> || true
 fi
