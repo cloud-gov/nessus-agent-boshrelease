7eec39f8da269b22dbfcf6096d5bcbdd5caf1fd6
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2018-11-26 19:10:08 -0500

    Ensure we link if manager is not linked

--- jobs/nessus-agent/templates/bin/link-agent.sh
+++ jobs/nessus-agent/templates/bin/link-agent.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # "Grep `nessuscli agent status` for "\[error\]" to prevent relinking all the time
-if /opt/nessus_agent/sbin/nessuscli agent status | grep '\[error\]' || ! /opt/nessus_agent/sbin/nessuscli agent status; then
+if /opt/nessus_agent/sbin/nessuscli agent status | grep '\[error\]' || \
+   /opt/nessus_agent/sbin/nessuscli agent status | grep "Not linked to a manager" || \
+   ! /opt/nessus_agent/sbin/nessuscli agent status; then
     /opt/nessus_agent/sbin/nessuscli agent link \
     --host=<%= p("nessus-agent.server") %> \
     --port=<%= p("nessus-agent.port") %> \
