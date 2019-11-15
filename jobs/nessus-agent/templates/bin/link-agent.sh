a9b9be8e4c9d8638bccf1dbe0174d5b1aa632061
Aaron Gershman <aegershman@gmail.com>
2019-11-15 14:04:54 -0600

    optional proxy arguments for linking agents

--- jobs/nessus-agent/templates/bin/link-agent.sh
+++ jobs/nessus-agent/templates/bin/link-agent.sh
@@ -8,6 +8,18 @@ if /opt/nessus_agent/sbin/nessuscli agent status | grep '\[error\]' || \
     --host=<%= p("nessus-agent.server") %> \
     --port=<%= p("nessus-agent.port") %> \
     --key=<%= p("nessus-agent.key") %> \
+    <% if_p('nessus-agent.proxy-host') do |proxy_host| %> \
+    --proxy-host="<%= proxy_host %>" \
+    <% end %> \
+    <% if_p('nessus-agent.proxy-port') do |proxy_port| %> \
+    --proxy-port="<%= proxy_port %>" \
+    <% end %> \
+    <% if_p('nessus-agent.proxy-username') do |proxy_username| %> \
+    --proxy-username="<%= proxy_username %>" \
+    <% end %> \
+    <% if_p('nessus-agent.proxy-password') do |proxy_password| %> \
+    --proxy-password="<%= proxy_password %>" \
+    <% end %> \
     --name=<%= spec.deployment %>-<%= name %>-<%= spec.index %>-<%= p("nessus-agent.group") %> \
     --groups=<%= p("nessus-agent.group") %> || true
 fi
